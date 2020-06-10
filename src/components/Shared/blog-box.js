712e795904db7c9cd96645a0c69c765915ad1406
cryptnotiq <43958441+cryptnotiq@users.noreply.github.com>
2020-06-10 14:09:39 +0200

    removed comments and likes icons

--- src/components/Shared/blog-box.js
+++ src/components/Shared/blog-box.js
@@ -18,10 +18,7 @@ class BlogBox extends Component {
                                 <h4><Link to="#" className="title text-dark">{blog.title}</Link></h4>
                                 <div className="post-meta mt-3">
                                     <Link to="#" className="text-muted float-right readmore">Read More <i className="mdi mdi-chevron-right"></i></Link>
-                                    <ul className="list-unstyled mb-0">
-                                        <li className="list-inline-item mr-2"><Link to="#" className="text-muted like"><i className="mdi mdi-heart-outline mr-1"></i>{blog.like}</Link></li>
-                                        <li className="list-inline-item"><Link to="#" className="text-muted comments"><i className="mdi mdi-comment-outline mr-1"></i>{blog.comment}</Link></li>
-                                    </ul>
+                                  
                                 </div>
                             </div>
                             <div className="author">
