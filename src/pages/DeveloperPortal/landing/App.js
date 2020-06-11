802b7284b77a8380a211560aa96e618177476ffd
cryptnotiq <43958441+cryptnotiq@users.noreply.github.com>
2020-06-11 04:44:57 +0200

    set up folder structure and finished tests

--- src/pages/DeveloperPortal/landing/App.js
+++ src/pages/DeveloperPortal/landing/App.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import AppBar from '@material-ui/core/AppBar';
 
 import NavBar from './navbar'
+import Landing from './landing'
+import CTA from './cta'
 
 
 function App() {
   return (
 
-
+<React.Fragment>
     <NavBar />
+    <Landing />
+    <CTA />
+
+    </React.Fragment>
+
   );
 }
 
--- src/pages/DeveloperPortal/landing/cta.js
+++ src/pages/DeveloperPortal/landing/cta.js
@@ -4,10 +4,10 @@ import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 
 
-export default function Landing() {
+export default function CTA() {
   return (
     <React.Fragment>
-    <Button>This is just the hello screen</Button>
+    <Button>This is where user chooses from 3/4 options with blocks.</Button>
 
     </React.Fragment>
   );
