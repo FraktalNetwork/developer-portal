6d56cb8a70e9b8c8a4f47d673d35e26384166169
cryptnotiq <43958441+cryptnotiq@users.noreply.github.com>
2020-07-02 14:17:54 +0200

    added content

--- src/pages/About/mission.js
+++ src/pages/About/mission.js
@@ -26,7 +26,8 @@ class Mission extends Component {
                             <Col md="6" className="mt-4 mt-sm-0 pt-2 pt-sm-0">
                                 <div className="section-title ml-lg-5">
                                     <h4 className="title mb-4">Clean And Modern Code</h4>
-                                    <p className="text-muted">This prevents repetitive patterns from impairing the overall visual impression and facilitates the comparison of different typefaces. Furthermore, it is advantageous when the dummy text is relatively realistic.</p>
+                                    <p className="text-muted">We achieve our mission by creating an environment where individuals and companies (those) who are passionate about enacting change can effectively work together to produce cutting edge technology and deliver it to those who can benefit from it the most.</p>
+                                    <p className="text-muted">While Fraktal produces some highly advanced technology, you do not need to have a technical background to join Fraktal and make a difference. There is a role for everyone to play to change the world with our technology (Fraktal) and make it a better place for everyone. </p>
                                     <Link to="#" className="btn btn-outline-primary">Start Now <i className="mdi mdi-chevron-right"></i></Link>
                                 </div>
                             </Col>
