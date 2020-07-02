4b09d17be452d1ec8b4c4e828caee3e24868c693
cryptnotiq <43958441+cryptnotiq@users.noreply.github.com>
2020-07-02 14:49:43 +0200

    designed article blocks

--- src/pages/About/articles.js
+++ src/pages/About/articles.js
@@ -28,16 +28,16 @@ class Articles extends Component {
             <React.Fragment>
                 <Container>
                 <div className="productstitle">
-                <h2 className="title mb-4">Products</h2>
+                <h2 className="title mb-4">Learn More</h2>
                 </div>
                 <div className="productstitle">
-                <p className="title mb-4">Individuals and companies love building on Fraktal Network and implementing it into their existing products.</p>
+                <p className="title mb-4">At the core of Fraktal is the Fraktal Network. An open financial system that can be used as an alternative to the banking system for storing money, sending money, and investing. Many different types of financial applications and services can be built on (using) Fraktal Network.</p>
                 </div>
                 <Row>
                     {
                         this.state.features.map((feature, key) =>
                             <Col lg={4} md={6} xs={12} key={key}>
-                                <Card className="features fea-primary rounded p-4 bg-light position-relative overflow-hidden border-0">
+                                <Card className="features fea-primary rounded p-4 shadow3d bg-light position-relative overflow-hidden border-0">
                                     <span className="h1 icon2 text-primary">
                                         <i className={feature.icon}></i>
                                     </span>
